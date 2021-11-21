fb894ba82809f153b391ebaaf93c819147d070ef
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-11-20 23:45:48 -0600

    deleted and added JS from kneelDiamonds.js file into the main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,29 @@ import { DiamondSizes } from "./DiamondSizes.js"
 
 const mainContainer = document.querySelector("#container")
 
-const renderAllHTML = () => {
-    mainContainer.innerHTML = KneelDiamonds()
-}
+const applicationHTML = `
+<h1>Kneel Diamonds</h1>
 
-renderAllHTML()
+<article class="choices">
+        <section class="choices__metals options">
+                <h2>Metals</h2>
+        </section>
+        <section class="choices__sizes options">
+                <h2>Sizes</h2>
+        </section>
+        <section class="choices__styles options">
+        <h2>Styles</h2>
+        </section>
+</article>
+
+<article>
+    <button id="orderButton">Create Custom Order</button>
+</article>
+
+<article class="customOrders">
+    <h2>Custom Jewelry Orders</h2>
+</article>
+`
+
+mainContainer.innerHTML = applicationHTML
 
