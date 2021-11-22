5e5bf5c65bc5dbe1801cb32c4bffcfffdd25bba2
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-11-21 22:32:10 -0600

    Added Code
    uncommented the code in main.js and move the js html back into kneelDiamonds.js. Also got the current jewelryStyles.js file to show up on the document

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -12,11 +12,12 @@ export const JewelryStyles = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray = styles.map(style => {
-        html += `<li>${style.style}</li>`
+    const listItemsArray = styles.map(
+        style => {
+        html += `<li id="style--${style.id}>${style.style}</li>`
     })
     // Join all of the strings in the array into a single string
-    html += listItems.join("")
+    html += listItemsArray.join("")
 
     html += "</ul>"
     return html
