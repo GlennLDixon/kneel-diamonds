8645a6a8e8222461f3b061da84064c145a2d6f84
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-11-22 20:11:45 -0600

    fixed the error within jewelryStyles.js on line 17 missing quotation mark

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -14,7 +14,7 @@ export const JewelryStyles = () => {
     // Use .map() for converting objects to <li> elements
     const listItemsArray = styles.map(
         style => {
-        html += `<li id="style--${style.id}>${style.style}</li>`
+        html += `<li id="style--${style.id}">${style.style}</li>`
     })
     // Join all of the strings in the array into a single string
     html += listItemsArray.join("")
