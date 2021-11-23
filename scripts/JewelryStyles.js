26bce9b8286ac4ec18222fbfe02f84f001f8fcb6
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-11-22 20:55:51 -0600

    added the if statement to display an alert showing the metal primary key

--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -14,8 +14,10 @@ export const JewelryStyles = () => {
     // Use .map() for converting objects to <li> elements
     const listItemsArray = styles.map(
         style => {
-        html += `<li id="style--${style.id}">${style.style}</li>`
-    })
+        return `<li>
+            <input type="radio" name="size" value="${style.id}"/>${style.style}
+        </li>`
+        })
     // Join all of the strings in the array into a single string
     html += listItemsArray.join("")
 
